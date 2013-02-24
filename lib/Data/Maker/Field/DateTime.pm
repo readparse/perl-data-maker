80cffbf7fd3cc78ac1a82fe4cc26b50f7e1db17d
John Ingram <john@funnycow.com>
2013-02-23 21:44:43 -0600

    added relative date support, so you can define one DateTime field that is either a fixed or random duration away from another DateTime field

--- lib/Data/Maker/Field/DateTime.pm
+++ lib/Data/Maker/Field/DateTime.pm
@@ -8,20 +8,53 @@ our $VERSION = '0.08';
 has start => ( is => 'rw');
 has end => ( is => 'rw');
 has format => ( is => 'rw');
+has relative_to => ( is => 'rw');
+has subtract => ( is => 'rw', isa => 'HashRef'); has add => ( is => 'rw', isa => 'HashRef');
 
 sub generate_value {
-  my $this = shift;
-  my $args = {
-    start => $this->parse_date_arg('start'),
-    end => $this->parse_date_arg('end'),
-  };
-  my $dt = DateTime::Event::Random->datetime(
-    %{$args}
-  );
+  my ($this, $maker) = @_;
+  my $dt;
+  if ($this->start && $this->end) {
+    my $args = {
+      start => $this->parse_date_arg('start'),
+      end => $this->parse_date_arg('end'),
+    };
+    $dt = DateTime::Event::Random->datetime(
+      %{$args}
+    );
+  } elsif (my $name = $this->relative_to) {
+    if (my $orig = $maker->in_progress($name)) {
+      if (ref($orig) eq 'DateTime') {
+        $dt = $orig->clone;
+        my $params;
+        if ($params = $this->subtract) {
+          my $new_params = check_params($params);
+          $dt->subtract_duration( DateTime::Duration->new( %{$new_params} ));
+        } elsif ($params = $this->add) {
+          my $new_params = check_params($params);
+          $dt->add_duration( DateTime::Duration->new( %{$new_params} ));
+        }
+      }
+    }
+  }
   if ($this->format) {
     return &{$this->format}($dt);
   }
   return $dt;
+}
+
+sub check_params {
+  my $params = shift;
+  my @keys = keys(%{$params});
+  my $key = shift @keys;	
+  my @values = values(%{$params});
+  my $value = shift @values;
+  if (my $ref = ref($value)) {
+    if ($ref eq 'ARRAY') {
+      return { $key => Data::Maker->random( $value ) };
+    }
+  }
+  return $params;
 }
 
 # `start` and `end` can be either a year or an actual DateTime object.  This method determines which it is and 
