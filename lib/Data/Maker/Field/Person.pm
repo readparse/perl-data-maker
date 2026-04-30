d2bff3d94f2e6786db7a43ad21ea2ae422b4b1e8
John Ingram <john@funnycow.com>
2026-04-30 07:24:51 -0700

    added the Email class to Data::Maker::Field::Person

--- lib/Data/Maker/Field/Person.pm
+++ lib/Data/Maker/Field/Person.pm
@@ -5,4 +5,5 @@ use Data::Maker::Field::Person::LastName;
 use Data::Maker::Field::Person::MiddleName;
 use Data::Maker::Field::Person::Gender;
 use Data::Maker::Field::Person::SSN;
+use Data::Maker::Field::Person::Email;
 1;
